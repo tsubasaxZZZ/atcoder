9aef7ee5da818d1bc8bcd6b367e6805030e0c9c2
tsunomur <tsunomur@microsoft.com>
2019-11-30 16:59:49 +0000

    Update

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <list>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <cmath>
+#include <iomanip>
+#include <functional>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int mod = 1000000007;
+const long long INF = 1LL << 55;
+const double EPS = 1e-10;
+
+int main()
+{
+    return 0;
+}
